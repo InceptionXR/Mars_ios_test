f7c036141dadb5ac18dc74ccd54707e71ebe5a2f
jack-sydorenko <jack@inceptionxr.com>
2022-06-30 16:25:44 +0300

    Set Camera direction explicitly

--- Assets/Scripts/MARSSessionController.cs
+++ Assets/Scripts/MARSSessionController.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using Unity.MARS.Data;
+using Unity.MARS.Providers;
+using Unity.XRTools.ModuleLoader;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public class MARSSessionController : MonoBehaviour
+    public class MARSSessionController : MonoBehaviour, IUsesCameraTexture
     {
+        IProvidesCameraTexture IFunctionalitySubscriber<IProvidesCameraTexture>.provider { get; set; }
 
+        private void Awake()
+        {
+            this.RequestCameraFacingDirection(CameraFacingDirection.User);
+        }
     }
 }
